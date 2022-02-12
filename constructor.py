fea1d031a4f41197c69f756ecaf5d8117a5b81c5
mohit1453 <67677509+mohit1453@users.noreply.github.com>
2022-02-12 16:54:33 +0530

    Create constructor.py

---
+++ constructor.py
@@ -0,0 +1,8 @@
+class Computer():
+    def __init__(self):
+        print("Hello to the computer world")
+    def  config(self):
+        print("5 gb RAM 128 GB Memory")
+a=Computer();
+a.config();
+
